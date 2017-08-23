1ddb76cfbc369c253b665662ad27cbc7a56b4bb6
teffcode <tefy.1@live.com>
2017-08-23 12:10:58 -0500

    initial directories

---
+++ src/features/components/CardView.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const CardView = () => (
+    <div/>
+);
+
+export default CardView;
---
+++ src/features/components/Layout.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Layout = () => (
+    <div/>
+);
+
+export default Layout;
